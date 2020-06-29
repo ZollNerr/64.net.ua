86b68fe909e6ffe7ec83816972962e0a893d3368
Dmitriy Zollnerr <2323282@gmail.com>
2020-06-29 19:49:11 +0300

    2st draft commit

--- js/burger.js
+++ js/burger.js
@@ -3,15 +3,24 @@ const menu = document.querySelector(".side-menu__list");
 const close = document.querySelector(".close-cross");
 
 burger.addEventListener('click', function () {
-    menu.classList.add('side-menu--active');
+  menu.classList.add('side-menu--active');
   console.log('нажали на бургер');
 });
 
 menu.addEventListener('click', function (e) {
-  if (!e.target.classList.contains('side-menu__link') || e.target.classList.contains('close')) {
+   
+    if (e.target.classList.contains('close')) {
+        // console.log('нажали на крестик');
+        console.log(e.target)
+        menu.classList.remove('side-menu--active');
+      }
+
+  if (!e.target.classList.contains('side-menu__link')) {
     menu.classList.remove('side-menu--active');
-    console.log('нажали на крестик');
+    console.log('нажали на пустое место');
   }
+
+
   if (e.target.classList.contains('side-menu__link')) {
     console.log('меняем секцию');
     menu.classList.remove('side-menu--active');
